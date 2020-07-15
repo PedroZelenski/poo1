c8689855083fc9c542b13fbb35e67ff68b41be89
Zelenski <zelenski.contato@gmail.com>
2020-07-15 10:38:22 -0300

    first commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO1
+{
+    class ContaBancaria
+    {
+        public string Conta;
+        public string Agencia;
+        public string Nome;
+        public string Senha = "senha123";
+        public int Saldo;
+
+        public ContaBancaria(int saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Depositar(int valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public int ConsultarSaldo()
+        {
+            int taxa = 10;
+            int valorTaxa = (Saldo * 10) / 100;
+            return Saldo - valorTaxa;
+        }
+
+        public void ImprimirSaldo()
+        {
+            Console.WriteLine($"{Nome}, aqui esta seu saldo(R$) da sua conta {Conta} na agencia {Agencia}: {ConsultarSaldo()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria(200);
+
+            contaBancaria.Agencia = "1234-5";
+            contaBancaria.Conta = "178328-8";
+            contaBancaria.Nome = "Pedro Zelenski";
+
+            contaBancaria.ImprimirSaldo();
+
+            contaBancaria.Depositar(200);
+            contaBancaria.ImprimirSaldo();
+            
+            contaBancaria.Depositar(300);
+            contaBancaria.ImprimirSaldo();
+            
+            contaBancaria.Sacar(100);
+            contaBancaria.ImprimirSaldo();
+
+        }
+    }
+}
